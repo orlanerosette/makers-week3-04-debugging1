3eb552c542a935ee11914877d43c1c311aeb5a8a
Orlane <orlanedoumbe@gmail.com>
2022-03-08 20:10:18 +0000

    Completed challenge

---
+++ lib/challenge.rb
@@ -0,0 +1,13 @@
+def get_most_common_letter(text)
+  counter = Hash.new(0)
+  text.chars.each do |char|
+    counter[char] += 1
+  end
+  counter
+  counter.to_a.sort_by { |k, v| v } [-2][0]
+end
+
+# Intended output:
+# 
+# > get_most_common_letter("the roof, the roof, the roof is on fire!")
+# => "o"
